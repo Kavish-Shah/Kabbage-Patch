c40d77427e3e46591051e4eae02389e44eceb94b
Kavish Shah <63824756+Kavish-Shah@users.noreply.github.com>
2025-09-13 00:41:35 -0700

    some more changes and mainly icon change

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -8,10 +8,10 @@ export function Hero() {
     return (
         <div className="flex flex-col items-center">
             <div className="py-8 mb-1 sm:mb-8 lg:mb-10">
-                <h1 className="text-gray-200 text-5xl md:text-6xl mb-4 font-league_spartan font-bold text-center md:text-left">
+                <h1 className="text-gray-200 text-5xl md:text-6xl mb-4 font-league_spartan font-bold text-center md:text-left hover:text-sky-300">
                     Kavish Shah
                 </h1>
-                <h2 className="text-xl md:text-2xl text-gray-200 font-quicksand text-center md:text-left"> Bioengineering Undergraduate at UCLA</h2>
+                <h2 className="text-xl md:text-2xl text-gray-200 font-quicksand text-center md:text-left hover:text-sky-300"> Bioengineering Undergraduate at UCLA</h2>
             </div>
             <div className="px-4 sm:px-6 md:px-0 w-full">
             <div className="max-w-screen-sm mx-auto flex flex-row items-center justify-between gap-6 sm:gap-[3em] overflow-visible mb-5 sm:mb-16 lg:mb-20">
